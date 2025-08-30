11a922ec116fe2fc234741b7835a36ef8e8b519f
Felipe Salazar <pipelon54@gmail.com>
2025-08-30 22:27:01 +0200

    Update theme colors to black in layout, browserconfig, and manifest files for a consistent dark theme.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -25,9 +25,9 @@ export default async function LocaleLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#3b82f6" />
+        <meta name="msapplication-TileColor" content="#000000" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#3b82f6" />
+        <meta name="theme-color" content="#000000" />
 
         <link rel="apple-touch-icon" href="/icons/icon-152x152.png" />
         <link
@@ -46,7 +46,7 @@ export default async function LocaleLayout({
         <link
           rel="mask-icon"
           href="/icons/safari-pinned-tab.svg"
-          color="#3b82f6"
+          color="#000000"
         />
         <link rel="shortcut icon" href="/favicon.ico" />
 
